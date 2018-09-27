f93c78f66c8b8ff7a23450fad6d2ea084c7b8fd1
AlexeyGITb <43606480+AlexeyGITb@users.noreply.github.com>
2018-09-28 00:01:13 +0300

    v 0.2

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,87 @@
 <template>
     <div id="app">
-
-
-
+<vue-snotify class="snotify centerTop"></vue-snotify>
+        <nav class="navbar"></nav>
         <div class="container-fluid">
             <div class="row">
+
                 <div class="col-sm-3 col-md-2 sidebar">
-                    <ul class="nav nav-sidebar">
+                    <ul class="nav nav-sidebar" style="position:fixed;">
+                        <!-- Checkboxes -->
                         <div class="form-check">
-                            <li v-for="activity in activities"><label><input type="checkbox" name="check" v-model="ids" :value="activity.id" /><span class="label-text">{{activity.name}}</span></label></li>
+                            <li v-for="activity in activities"><label><input type="checkbox" v-model="checkboxesArray" :value="activity.id" /><span class="label-text">{{activity.name}}</span></label></li>
                         </div>
                     </ul>
                 </div>
+                <!---->
                 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                    <button type="button" class="btn btn-primary btn-lg createT" data-toggle="modal" data-target="#exampleModal">Create Trip</button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="text-center modal-title" id="exampleModalLabel">Select Trip id from list below</h5>
 
-
+                                </div>
+                                <div class="modal-body">
+                                    <select class="form-control" v-model="tripId">
+                                        <option v-for="trip in trips" :value="trip.id">{{trip.id}} — {{trip.name}}</option>
+                                    </select>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal" @click="sendData(); displayNotification();">Create trip</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!---->
                     <ul>
-                        <li v-for="(trip,index) in trips" v-model="check":key="index">
+                        <!-- Trips-->
+                        <li v-for="(trip,index) in trips" :key="index">
                             <div class="container  trip shadow">
                                 <div class="row">
-                                    <div class="col-4 test">
-                                        <img :src="trip.imageUrl" class="img-fluid  tripImg" alt="Responsive image"></div>
+
+                                    <div class="col-4 tripLeft">
+                                        <img :src="trip.imageUrl" :id="'img'+trip.id" class="tripImg" alt="Responsive image"></div>
+
                                     <div class="col-8">
-                                        <h3 class=" text-center border-primary border-bottom text-primary ">{{trip.name}},{{trip.id}}</h3>
+                                        <!--main -->
+                                        <h3 class=" text-center border-primary border-bottom text-primary ">{{trip.name}}</h3>
                                         <p>{{trip.destination.name}} </p>
                                         <p><span class="text-primary">Pick up:</span> {{new Date(trip.pickupTime).toLocaleString()}}</p>
                                         <p><span class="text-primary">Event start:</span> {{new Date(trip.dateTrip).toLocaleString()}}</p>
                                         <p><span class="text-primary">Drop off:</span> {{new Date(trip.dropOffTime).toLocaleString()}}</p>
-
+                                        <!---->
+                                        <!--activity icons-->
                                         <ul>
-               <li v-for="(activity,i) in trip.activityTypes" class="activityTypes"><img class="actImg " :src="trip.activityTypes[i].picUrl"></li>
+                                            <li v-for="(activity,i) in trip.activityTypes" class="activityTypes">
+                                                <img class="actImg" :src="trip.activityTypes[i].picUrl">
+                                            </li>
                                         </ul>
-                                        <!--
-                                        <img class="actImg" :src="trip.activityTypes[0].picUrl">
-                                        <img class="actImg" :src="trip.activityTypes[1].picUrl">
--->
-                                        <!--                                        <img class="actImg" :src="trip.activityTypes[2].picUrl"> -->
+                                        <!---->
+                                        <!--description-->
                                         <div class="panel-group" id="accordion">
                                             <div class="panel panel-default">
-                                                <div class="panel-heading">
-                                                    <h4 class="panel-title">
-                                                        <a data-toggle="collapse" class="butt" data-parent="#accordion" @:click="bot-pannel" :href="'#trip' + index"><img class="arrow " src="src/assets/arrow.png">
-                                                        </a>
-                                                    </h4>
-                                                </div>
+                                               
+                                                <a data-toggle="collapse" class="link" data-parent="#accordion" :href="'#trip' + index"><img class="arrow align-middle" src="src/assets/arrow.png">
+                                                </a>
                                                 <div :id="'trip' + index" class="panel-collapse collapse in">
-                                                    <div class="panel-body">
-                                                        <div class="row">
-                                                            <div class="col-12">
-                                                                <p>{{trip.activityDescription}}</p>
-                                                            </div>
+                                                    <div class="panel-body row">
+                                                        <div class="col-12">
+                                                            <p>{{trip.activityDescription}}</p>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
+                                        <!---->
                                     </div>
                                 </div>
-
                             </div>
                         </li>
-
                     </ul>
-
                 </div>
             </div>
         </div>
@@ -71,244 +90,212 @@
 </template>
 
 <script>
-
-
     export default {
         name: 'app',
         data: () => ({
             activities: [],
-            trips:[],
-            ids:[],
-            activ:[],
-            d:[]
-
+            trips: [],
+            checkboxesArray: [],
+            activ: [],
+            oldTrips: [],
+            tripId: ''
         }),
-//            check(){
-//                 if (this.ids.length == 0)
-//                return trips;
-//                else {
-////                   for(var i=0; this.ids.length; i++){
-////                       console.log(this.ids[i]);
-//                axios.get('http://qa.ridj-it.com/app/trip?activity=' + this.ids[i]).then(response => {
-//                    this.temp.push(responce.data);
-//                })
-//                                                                                         
-//                                                                                         }
-//              // handle success
-//    
-//            this.trips =temp.data;
-//                       
-//                }
-//                            this.temp=[];
-//                }
-         methods: {
-           check() {
-                if (this.ids.length == 0){
-                axios.get('http://qa.ridj-it.com/app/trip').then(response => {
-                    console.log(this.trips)
-                    this.trips = response.data
-                })}
-                        else{                                            
-                axios.get('http://qa.ridj-it.com/app/trip?activity=' + this.ids).then(response => {
-                // handle success
-                    var d = new Date(+milliseconds);
-                return  this.trips = response.data;
-                                })}
-                          }
+        methods: {
+            displayNotification() { 
+        this.$snotify.success("Trip successfully created!");
+    },
+            sendData() {  //create new trip
+                var test = {
+                    tripId: this.tripId
+                }
+                var json = JSON.stringify(test);
+                axios({
+                        method: 'POST',
+                        url: 'http://qa.ridj-it.com/app/trip',
+                        data: json,
+                        headers: {
+                            'Content-Type': 'application/json'           
+                        }
+                    })
+                    .then((response) => {
+                        console.log(response);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
             },
-
-        mounted() {
-            axios.get('http://qa.ridj-it.com/app/activity').then(response => {  
-                // handle success
+            loadTrips() { //filtering thru API (currently working only with one active checkbox)
+                if (this.checkboxesArray.length) {
+                    const urlParams = this.checkboxesArray.map(item => {
+                        return `activity=${item}`
+                    }).join('&')
+                    console.log(`http://qa.ridj-it.com/app/trip?${urlParams}`);
+                    axios.get(`http://qa.ridj-it.com/app/trip?${urlParams}`)
+                        .then(response => {
+                            this.trips = response.data
+                        })
+                } else {
+                    axios.get('http://qa.ridj-it.com/app/trip')
+                        .then(response => {
+                            this.trips = response.data
+                        })
+                }
+            }
+        },
+        beforeMount() {
+            this.loadTrips()  //Filing checkboxes thru api
+            axios.get('http://qa.ridj-it.com/app/activity').then(response => {
                 this.activities = response.data
             })
-            axios.get('http://qa.ridj-it.com/app/trip').then(response => {
-                // handle success
-//                response.data.trips.dateTrip
-                this.trips = response.data
-                
-            })
+        },
+        watch: {
+            checkboxesArray() { 
+                this.loadTrips();
+            }
         }
     }
+
 </script>
 
 <style>
-    ul{
+    @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+    @import 'normalize.css';
+
+    .createT {
+        margin-left: 78%;
+        width: 160px;
+    }
+    p{
+        font-size: 1.3em;
+    }
+
+    ul {
         margin: 0;
-        padding: 0;
+        padding: 0
     }
-    .activityTypes{
-        display: inline;
+
+    .activityTypes {
+        display: inline
     }
-    li{
-         list-style-type:none;
+
+    li {
+        list-style-type: none
     }
-    body{
-        background-color: ghostwhite;
+
+    body {
+        background-color: #f8f8ff
     }
-    .arrow{
-        margin-left:260px;
-        margin-bottom: 20px;
+
+    .arrow {
         height: 20px;
         width: 20px;
+        margin-left: 50%;
+        padding-bottom:2px;
     }
-    .butt{
-        position: absolute;
-        margin: -20px;
+
+    .link {
+        display: block
     }
-    .tripImg{
-    max-width: 100%;
-        min-height:200px;
+
+    .tripImg {
+        height: 240px;
+        width: inherit;
         border-top-left-radius: 20px;
-          border-bottom-left-radius: inherit;
+        border-bottom-left-radius: 20px;
     }
-    .tripImgO{
-            max-width: 100%;
-        min-height:200px;
-        border-top-left-radius: 20px;
-         border-bottom-left-radius: 0;
-        
+
+    .actImg {
+        width: 50px;
+        height: 50px
     }
-      .actImg{
-   width: 50px;
-          height: 50px;
-    }
-    @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-    .test{
+
+    .tripLeft {
         margin: 0;
         padding: 0;
         border-top-left-radius: 23px;
         background-color: #007bff;
-           border-bottom-left-radius: 20px;
+        border-bottom-left-radius: 20px
     }
-    .trip{
+
+    .trip {
         min-height: 200px;
-     width: 800px;
+        width: 80%;
         margin-top: 10px;
         border-radius: 20px;
-        background-color: white;
-    }       
-p{
-    font-family: "FontAwesome";
-    color: #898686;
-    padding: 0;
-    margin: 0;
+        background-color: #fff
+    }
+
+    p {
+        font-family: "FontAwesome";
+        color: #898686;
+        padding: 0;
+        margin: 0
     }
 
     label {
         position: relative;
         cursor: pointer;
         color: #898686;
-        font-size: 20px;
+        font-size: 25px;
     }
 
-    input[type="checkbox"]{
+    input[type="checkbox"] {
         position: absolute;
-        right: 9000px;
+        right: 9000px
     }
 
-    /*Check box*/
     input[type="checkbox"]+.label-text:before {
         content: "\f096";
         font-family: "FontAwesome";
         speak: none;
         font-style: normal;
-        font-weight: normal;
+        font-weight: 400;
         font-variant: normal;
         text-transform: none;
         line-height: 1;
         -webkit-font-smoothing: antialiased;
         width: 1em;
         display: inline-block;
-        margin-right: 5px;
+        margin-right: 5px
     }
 
     input[type="checkbox"]:checked+.label-text:before {
         content: "\f14a";
         color: #2980b9;
-        animation: effect 250ms ease-in;
+        animation: effect 250ms ease-in
     }
 
     input[type="checkbox"]:disabled+.label-text {
-        color: #aaa;
+        color: #aaa
     }
 
     input[type="checkbox"]:disabled+.label-text:before {
         content: "\f0c8";
-        color: #ccc;
+        color: #ccc
     }
-
 
     @keyframes effect {
         0% {
-            transform: scale(0);
+            transform: scale(0)
         }
 
         25% {
-            transform: scale(1.3);
+            transform: scale(1.3)
         }
 
         75% {
-            transform: scale(1.4);
+            transform: scale(1.4)
         }
 
         100% {
-            transform: scale(1);
+            transform: scale(1)
         }
     }
 
 </style>
 
-
-
-
-
-
-
-
-// },
-// computed: { filter:function (activ){
-// axios.get('http://qa.ridj-it.com/app/trip?activity=' + this.activ).then(response => {
-// // handle success
-// this.trips = response.data;
-// })
-// }
-// },
-
-
-
-
-
-
-// components: {'test', {
-// data: function () {
-// return {
-// activ: ""
-// }
-// },
-// method:{
-// check: function(activ){
-// this.id =
-// axios.get('http://qa.ridj-it.com/app/trip?activity=' + this.activ).then(response => {
-// // handle success
-// this.trips = response.data;
-// })
-// }
-//
-// },
-// template: '<li><label><input type="checkbox" name="check" :checked="check" :value="activity.id" /><span class="label-text">{{activity.name}}</span></label></li>'
-//})
-// }
-
-
-
-
-
-
-
-
-
-
+<!--
+            Filtering thru js (partially working)
 // <li v-for="activity in activities"><label><input type="checkbox" name="check" v-model="activ" :value="activity.id" />
         // data: () => ({
         // activ: []
@@ -319,3 +306,4 @@ p{
         // return this.trips.filter(j => this.activ.includes(j.activityTypes[0].id))
         // }
         // },
+-->
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+// You also need to import the styles. If you're using webpack's css-loader, you can do so here:
+import 'vue-snotify/styles/material.css'; // or dark.css or simple.css
+
+
+import Snotify, { SnotifyPosition } from 'vue-snotify'
+
+const options = {
+  toast: {
+    position: SnotifyPosition.centerTop
+  }
+}
+
+Vue.use(Snotify, options)
+
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
