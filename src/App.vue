398f4d7d0394f9bbf473f2add837fe82326c466b
AlexeyGITb <43606480+AlexeyGITb@users.noreply.github.com>
2018-09-30 03:20:31 +0300

    0.3

--- src/App.vue
+++ src/App.vue
@@ -37,55 +37,55 @@
                     </div>
                     <!---->
                     <div class="container ">
-                                           <div class="container clearfix" style="">
-                                            <button type="button" class="btn btn-primary btn-lg pull-right createT" data-toggle="modal" data-target="#Modal">Create Trip</button>
+                        <div class="container clearfix" style="">
+                            <button type="button" class="btn btn-primary btn-lg pull-right createT" data-toggle="modal" data-target="#Modal">Create Trip</button>
 
-    </div>
-                    <ul>
-                        <!-- Trips-->
-                        <li v-for="(trip,index) in trips" :test="trip.id" :key="index">
-                            <div class="container  trip shadow">
-                                <div class="row">
+                        </div>
+                        <ul>
+                            <!-- Trips-->
+                            <li v-for="(trip,index) in trips" :key="index">
+                                <div class="container  trip shadow">
+                                    <div class="row">
 
-                                    <div class="col-4 tripLeft">
-                                        <img :src="trip.imageUrl" :id="'img'+trip.id"  class="tripImg" alt="Responsive image"></div>
+                                        <div class="col-4 tripLeft">
+                                            <img :src="trip.imageUrl" :id="'img'+trip.id" class="tripImg" alt="Responsive image"></div>
 
-                                    <div class="col-8">
-                                        <!--main -->
-                                        <h3 class=" text-center border-primary border-bottom text-primary ">{{trip.name}}</h3>
-                                        <p>{{trip.destination.name}} </p>
-                                        <p><span class="text-primary">Pick up:</span> {{new Date(trip.pickupTime).toLocaleString()}}</p>
-                                        <p><span class="text-primary">Event start:</span> {{new Date(trip.dateTrip).toLocaleString()}}</p>
-                                        <p><span class="text-primary">Drop off:</span> {{new Date(trip.dropOffTime).toLocaleString()}}</p>
-                                        <!---->
-                                        <!--activity icons-->
-                                        <ul>
-                                            <li v-for="(activity,i) in trip.activityTypes" class="activityTypes">
-                                                <img class="actImg" :src="trip.activityTypes[i].picUrl">
-                                            </li>
-                                        </ul>
-                                        <!---->
-                                        <!--description-->
-                                        <div class="panel-group" id="accordion">
-                                            <div class="panel panel-default">
+                                        <div class="col-8">
+                                            <!--main -->
+                                            <h3 class=" text-center border-primary border-bottom text-primary ">{{trip.name}}</h3>
+                                            <p>{{trip.destination.name}} </p>
+                                            <p><span class="text-primary">Pick up:</span> {{new Date(trip.pickupTime).toLocaleString()}}</p>
+                                            <p><span class="text-primary">Event start:</span> {{new Date(trip.dateTrip).toLocaleString()}}</p>
+                                            <p><span class="text-primary">Drop off:</span> {{new Date(trip.dropOffTime).toLocaleString()}}</p>
+                                            <!---->
+                                            <!--activity icons-->
+                                            <ul>
+                                                <li v-for="(activity,i) in trip.activityTypes" class="activityTypes">
+                                                    <img class="actImg" :src="trip.activityTypes[i].picUrl">
+                                                </li>
+                                            </ul>
+                                            <!---->
+                                            <!--description-->
+                                            <div class="panel-group" id="accordion">
+                                                <div class="panel panel-default">
 
-                                                <a data-toggle="collapse"  class="link" data-parent="#accordion" :href="'#trip' + index"><img class="arrow align-middle" src="src/assets/arrow.png"></a>
-                                                <div :id="'trip' + index" class="panel-collapse collapse in">
-                                                    <div class="panel-body row">
-                                                        <div class="col-12">
-                                                            <p>{{trip.activityDescription}}</p>
+                                                    <a data-toggle="collapse" :trid="trip.id" @click="testmet" class="link" data-parent="#accordion" :href="'#trip' + index"><img class="arrow align-middle" :id="'aid' + trip.id" src="src/assets/arrow1.png"></a>
+                                                    <div :id="'trip' + index" class="panel-collapse collapse in">
+                                                        <div class="panel-body row">
+                                                            <div class="col-12">
+                                                                <p>{{trip.activityDescription}}</p>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <!---->
                                         </div>
-                                        <!---->
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                    </ul>
-                                        </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -94,25 +94,70 @@
 </template>
 
 <script>
-
     export default {
-        props:['test'],
         name: 'app',
         data: () => ({
-            index:'',
-            test:'',
+            index: '',
+            test: '',
             activities: [],
             trips: [],
             checkboxesArray: [],
             activ: [],
             oldTrips: [],
-            tripId: ''
+            tripId: '',
         }),
-        methods: {
 
+        methods: {
+            //ser(){
+            //     axios.get("http://qa.ridj-it.com/app/3218/member",{
+            //                         }) .then((response) => {
+            //                        console.log(response);
+            //                    })
+            //                    .catch((error) => {
+            //                        console.log(error);
+            //                    });
+            //},
+
+            testmet: function(event) {
+                var test = event.target.getAttribute('trid');
+                console.log(test + "next?")
+
+                //arrow flip           
+                if ($('#aid' + test).css('transform') == 'none') {
+                    $('#aid' + test).css({
+                        'transform': 'rotate(180deg)',
+                        '-moz-transition': 'transform 0.5s',
+                        '-webkit-transition': 'transform 0.5s',
+                        'transition': 'transform 0.5s'
+                    });
+                } else {
+                    $('#aid' + test).css({
+                        'transform': '',
+                        '-moz-transition': 'transform 0.5s',
+                        '-webkit-transition': 'transform 0.5s',
+                        'transition': 'transform 0.5s'
+                    });
+                };
+                //img border
+                if ($('#img' + test).css('border-bottom-left-radius') == '20px') {
+                    $('#img' + test).css({
+                        'border-bottom-left-radius': '0'
+                    });
+                } else {
+                    $('#img' + test).css({
+                        'border-bottom-left-radius': '20px',
+                        '-moz-transition': 'border-bottom-left-radius 0.7s',
+                        '-webkit-transition': 'border-bottom-left-radius 0.7s',
+                        'transition': 'border-bottom-left-radius 0.7s'
+                    });
+                }
+                
+            },
             displayNotification() {
                 this.$snotify.success("Trip successfully created!");
             },
+
+
             sendData() { //create new trip
                 var test = {
                     tripId: this.tripId
@@ -152,6 +197,27 @@
             }
         },
         beforeMount() {
+            var data = "username=qa_test@test.ru&password=1234qwer";
+            axios("http://qa.ridj-it.com/login", {
+                    "method": 'POST',
+                    "async": true,
+                    "crossDomain": true,
+                    data: data,
+                    headers: {
+                        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                        "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+                        "content-type": "application/x-www-form-urlencoded",
+
+                    }
+
+                }).then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+
             this.loadTrips() //Filing checkboxes thru api
             axios.get('http://qa.ridj-it.com/app/activity').then(response => {
                 this.activities = response.data
@@ -163,6 +229,7 @@
             }
         }
     }
+
 </script>
 
 <style>
@@ -173,9 +240,8 @@
         font-family: "FontAwesome";
     }
 
-    .main{
-    
-    }
+    .main {}
+
     .createT {
         margin-right: 9%;
         width: 160px;
@@ -203,6 +269,7 @@
     }
 
     .arrow {
+        pointer-events: none;
         height: 20px;
         width: 20px;
         margin-left: 50%;
@@ -305,6 +372,7 @@
             transform: scale(1)
         }
     }
+
 </style>
 
 <!--
